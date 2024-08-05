7b8229d44fdb23e20c35536ff74ac549dc72461b
Ghannam Aljabari <galjabari@gmail.com>
2024-08-05 10:32:52 +0300

    .

--- swarm/create_nodes.tf
+++ swarm/create_nodes.tf
@@ -1,6 +1,6 @@
-resource "proxmox_lxc" "manager" {
+resource "proxmox_lxc" "manager1" {
   target_node     = "pve"
-  hostname        = "manager"
+  hostname        = "manager1"
   ostemplate      = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
   password        = "ubuntu"
   unprivileged    = true
